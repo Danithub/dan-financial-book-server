bb5f84eb50c97af298ff605b671e6c144cfee041
DAN <life4789@gmail.com>
2024-12-03 09:35:56 +0900

    build: 패키징 타입 변경
    war 파일 패키징을 위해 Dependency 추가 및 main 함수 수정

--- src/main/java/dan/example/dan_financial_book/DanFinancialBookApplication.java
+++ src/main/java/dan/example/dan_financial_book/DanFinancialBookApplication.java
@@ -3,13 +3,19 @@ package dan.example.dan_financial_book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "dan.example.dan_financial_book")
 @MapperScan(basePackages = "dan.example.dan_financial_book")
-public class DanFinancialBookApplication {
+public class DanFinancialBookApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DanFinancialBookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DanFinancialBookApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DanFinancialBookApplication.class);
+    }
 }
