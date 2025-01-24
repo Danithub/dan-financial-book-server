7a061879c1a993d828854ac6d8313a9eced674cf
DAN <life4789@gmail.com>
2025-01-24 15:45:48 +0900

    Build: CORS 예외 처리
    local vue port 예외처리

--- src/main/java/dan/example/dan_financial_book/common/config/WebConfig.java
+++ src/main/java/dan/example/dan_financial_book/common/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://15.165.86.111")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
